0298fee42e42e8771860e6bf070cfa7b1aafba4c
ScrawnyColeman <thomashutchinson450@gmail.com>
2021-11-18 18:59:20 +0000

    redirect

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const Product = ({ name, price, mainImg}) => {
+    const sendToPay = () => {
+        window.location.href = `https://trilo.us/thomas/${price.high}`;
+    }
     return (
-        <div>
+        <div onClick={sendToPay}>
             {name}
             {price.high}
             <img width={128} src={mainImg}/>
